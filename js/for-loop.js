87373f8e5ef0854e038e6900082d182951f6a5b1
Silavsale <silavsale@gmail.com>
2019-03-10 18:46:19 -0400

    first commit

---
+++ js/for-loop.js
@@ -0,0 +1,56 @@
+// --------Exercise #1
+// let number = +prompt('Enter a number', '');
+//
+// for (let i = 1; i<= 10; i++){
+//     console.log(`${number} x ${i} = ${number * i}`);
+//
+// }
+//
+// let divIdName2;
+// let newdiv2;
+// for (let i = 1; i<= number; i++){
+//
+//     newdiv2 = document.createElement('div');
+//     newdiv2.setAttribute('id',divIdName2);
+//     // newdiv2.innerHTML ='<div id="box1'+ i +'">111112</div>';
+//     newdiv2.innerHTML =`Number = ${number} x ${i} = ${number}`;
+//
+//     // document.getElementById("for-loop").innerHTML = (`${number} x ${i} = ${number}`);
+//     document.body.appendChild(newdiv2);
+// }
+
+// --------Exercise #2
+// let number1 = +prompt('Enter first number', '');
+// let number2 = +prompt('Enter second number', '');
+//
+// let sum = 0;
+// let index = number2;
+//
+// for (let i = number1; i <= index; i++) {
+//     if (number2 % 2 == 0){
+//        sum += number2;
+//    }
+//     console.log(`result = ${sum} ,number3 = ${number2}`);
+//     number2 --;
+// }
+// console.log(`final result = ${sum}`);
+
+// --------Exercise #3
+// let number1 = +prompt('Enter first number', '');
+// let sum = 0;
+// while (number1 > 0){
+//     sum += number1;
+//     console.log(`Sum = ${sum} , number1 = ${number1}`);
+//     number1 --;
+// }
+// console.log(`Sum = ${sum}`);
+
+// --------Exercise #4
+let number1;
+let sum = 0;
+while (number1 !== 0){
+    number1 = +prompt('Enter first number', '');
+    sum += number1;
+    console.log(`Sum = ${sum} , number1 = ${number1}`);
+}
+console.log(`Sum = ${sum}`);
